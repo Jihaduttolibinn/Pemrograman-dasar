09592b31cfef6898a3baa8200648d982c8355d2a
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-01 14:02:20 +0800

    Add project files.

---
+++ Pemrograman dasar/Pemrograman dasar.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim : " << mhs.nim;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+
+
+
+
+
+
+
+    
+}
+
